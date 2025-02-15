18c23f74003709ecb739b79741ebdd2376f5c5ca
Manuel Davila <manuel.davila3@educa.madrid.org>
2025-02-15 18:14:58 +0100

    Ejercicio 5

--- exercise3/start/Astronomy/Astronomy/Pages/AstronomicalBodyPage.xaml.cs
+++ exercise3/start/Astronomy/Astronomy/Pages/AstronomicalBodyPage.xaml.cs
@@ -1,35 +1,45 @@
-namespace Astronomy.Pages;
-
-public partial class AstronomicalBodyPage : ContentPage
+namespace Astronomy.Pages
 {
-
-    public AstronomicalBodyPage()
+    [QueryProperty(nameof(AstroName), "astroName")]
+    public partial class AstronomicalBodyPage : ContentPage
     {
-        InitializeComponent();
-    }
-
-    void UpdateAstroBodyUI(string astroName)
-    {
-        AstronomicalBody body = FindAstroData(astroName);
-
-        Title = body.Name;
-
-        lblIcon.Text = body.EmojiIcon;
-        lblName.Text = body.Name;
-        lblMass.Text = body.Mass;
-        lblCircumference.Text = body.Circumference;
-        lblAge.Text = body.Age;
-    }
-
-    AstronomicalBody FindAstroData(string astronomicalBodyName)
-    {
-        return astronomicalBodyName switch
+        string astroName;
+        public string AstroName
         {
-            "comet" => SolarSystemData.HalleysComet,
-            "earth" => SolarSystemData.Earth,
-            "moon" => SolarSystemData.Moon,
-            "sun" => SolarSystemData.Sun,
-            _ => throw new ArgumentException()
-        };
+            get => astroName;
+            set
+            {
+                astroName = value;
+                UpdateAstroBodyUI(astroName);
+            }
+        }
+
+        public AstronomicalBodyPage()
+        {
+            InitializeComponent();
+        }
+
+        void UpdateAstroBodyUI(string astroName)
+        {
+            AstronomicalBody body = FindAstroData(astroName);
+            Title = body.Name;
+
+            lblIcon.Text = body.EmojiIcon;
+            lblName.Text = body.Name;
+            lblMass.Text = body.Mass;
+            lblCircumference.Text = body.Circumference;
+            lblAge.Text = body.Age;
+        }
+        AstronomicalBody FindAstroData(string astronomicalBodyName)
+        {
+            return astronomicalBodyName switch
+            {
+                "comet" => SolarSystemData.HalleysComet,
+                "earth" => SolarSystemData.Earth,
+                "moon" => SolarSystemData.Moon,
+                "sun" => SolarSystemData.Sun,
+                _ => throw new ArgumentException("Cuerpo astronómico no válido")
+            };
+        }
     }
 }
